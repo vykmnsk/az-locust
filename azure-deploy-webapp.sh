b960b45b086bcce52fb3b647ff34c1b78217bfd2
Yuri <YKlind@bunnings.com.au>
2020-09-12 21:54:23 +1000

    Run flask hello-world on azure

--- azure-deploy-webapp.sh
+++ azure-deploy-webapp.sh
@@ -4,35 +4,58 @@
 set -euo pipefail
 
 echo "starting..."
-LOG="azure.log"
-cat << EOF > $LOG
+log="azure.log"
+cat << EOF > $log
 EOF
 
 # config
-AZ_RESOURCE_GROUP=$PLT_AZ_RESOURCE_GROUP
-AZ_LOCATION=$PLT_AZ_LOCATION
-AZ_STORAGE_ACCOUNT=$PLT_AZ_STORAGE_ACCOUNT
+resource_group=$PLT_AZ_RESOURCE_GROUP
+location=$PLT_AZ_LOCATION
+storage_account=$PLT_AZ_STORAGE_ACCOUNT
+app_src_dir="webapp"
+app_service_plan=$PLT_AZ_APP_SERVICE_PLAN
+web_app=$PLT_AZ_WEB_APP
 
-echo "create resource group: $AZ_RESOURCE_GROUP" | tee -a $LOG
+echo "prepare app src zip"
+7z a $app_src_dir.zip ./$app_src_dir/*.* > $log  # use zip for *nix
+
+echo "create resource group: $resource_group" | tee -a $log
 az group create \
-    --name $AZ_RESOURCE_GROUP \
-    --location $AZ_LOCATION \
-    -o json >> $LOG	
+    -n $resource_group --location $location \
+    -o json >> $log	
 
-echo "create storage account: $AZ_STORAGE_ACCOUNT" | tee -a $LOG
+echo "create storage account: $storage_account" | tee -a $log
 az storage account create \
-    --name $AZ_STORAGE_ACCOUNT \
-    -g $AZ_RESOURCE_GROUP \
+    -g $resource_group -n $storage_account \
     --sku Standard_LRS \
-    -o json >> $LOG
+    -o json >> $log
 
-echo "get storage connection string" | tee -a $LOG
-AZ_STORAGE_CONN_STRING=$(az storage account show-connection-string -g $AZ_RESOURCE_GROUP -n $AZ_STORAGE_ACCOUNT -o tsv)
+echo "get storage connection string" | tee -a $log
+storage_conn_string=$(az storage account show-connection-string -g $resource_group -n $storage_account -o tsv)
+echo "conn str=$storage_conn_string"
 
-echo "conn str=$AZ_STORAGE_CONN_STRING"
+echo "creating app service plan" | tee -a $log
+az appservice plan create \
+    -g $resource_group -n $app_service_plan \
+    --is-linux --sku F1 \
+    -o json >> $log
 
-echo "creating app service plan"
-az appservice plan create -n $PLT_APP_SERVICE_PLAN -g $PLT_AZ_RESOURCE_GROUP --is-linux --sku F1
+echo "deploying web app" | tee -a $log
+az webapp create \
+    -g $resource_group -p $app_service_plan -n $web_app \
+    --runtime "python|3.6" \
+    -o json >> $log
 
-echo "creating web app"
-az webapp create -n $PLT_WEB_APP -g $PLT_AZ_RESOURCE_GROUP -p $PLT_APP_SERVICE_PLAN --deployment-local-git --runtime "python|3.6"
+az webapp config appsettings set \
+    -g $resource_group -n $web_app \
+    --settings SCM_DO_BUILD_DURING_DEPLOYMENT=true \
+    -o json >> $log
+
+az webapp deployment source config-zip \
+    -g $resource_group -n $web_app \
+    --src ./$app_src_dir.zip \
+    -o json >> $log
+
+
+echo "Go to:" | tee -a $log
+echo "http://$web_app.azurewebsites.net" | tee -a $log
--- webapp/app.py
+++ webapp/app.py
@@ -1,32 +1,6 @@
-from flask import Flask, request, abort
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-from azure.core.exceptions import ResourceExistsError
-import os
-
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/feed/", methods=['GET'])
-def feed():
-    uniq = request.args.get('uniq')
-    if not uniq:
-        abort(400)    
-        
-    blob_name = f'file_{uniq}.txt'    
-    content = f"test content {uniq}"
-        
-    conn_str = os.environ['PLT_AZ_STORAGE_CONN_STRING']
-    container_name = os.environ['PLT_AZ_CONTAINER_NAME']
-   
-        
-    blobservice_client = BlobServiceClient.from_connection_string(conn_str)
-    try:
-        blobservice_client.create_container(container_name)
-    except ResourceExistsError:
-        pass
-        
-    blob_client = blobservice_client.get_blob_client(container=container_name, blob=blob_name)
-    blob_client.upload_blob(content)
-        
-    return f"Fed {uniq}."
-
+@app.route("/")
+def hello():
+    return "Hello from Azure!"
